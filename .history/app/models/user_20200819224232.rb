ce536f95500c0c60bc61520ca1fb4aecac31e732
hiroaki-kitagawa <hirock17l@icloud.com>
2020-08-19 23:20:31 +0900

    Implement basic login

--- .history/app/models/user_20200819224232.rb
+++ .history/app/models/user_20200819224232.rb
@@ -11,7 +11,5 @@ class User < ApplicationRecord
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
   end
 end
