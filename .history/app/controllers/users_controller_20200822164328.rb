8ed093ede26bd7278b2ddc351512d3fe02531722
hiroaki-kitagawa <hirock17l@icloud.com>
2020-08-22 18:54:41 +0900

    Finish user edit, update, index, and destroy actions

---
+++ .history/app/controllers/users_controller_20200822164328.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def upadte
+    user = User.find(params[:id])
+    if @user.update(user_params)
+      # 更新に成功した場合を扱う、
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+end
