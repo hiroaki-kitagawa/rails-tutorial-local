9f86ccd2b140173c634dda9d8e935d06141584e2
hiroaki-kitagawa <hirock17l@icloud.com>
2020-08-23 16:37:49 +0900

    Add account activation

--- .history/app/models/user_20200823132743.rb
+++ .history/app/models/user_20200823132743.rb
@@ -29,39 +29,13 @@ class User < ApplicationRecord
   end
 
   # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
-    return false if digest.nil?
-    BCrypt::Password.new(digest).is_password?(token)
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
   # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
-  end
-
-  # アカウントを有効にする
-  def activate
-    update_attribute(:activated, true)
-    update_attribute(:activated_at, Time.zone.now)
-  end
-
-  # 有効化用のメールを送信する
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
-  end
-
-  private
-
-
-  # メールアドレスを全て小文字にする
-  def downcase_email
-    self.email = email.downcase
-  end
-
-  # 有効かトークンとダイジェストを作成および代入する
-  def create_activation_digest
-    self.activation_token  = User.new_token
-    self.activation_digest = User.digest(activation_token)
   end
 end
